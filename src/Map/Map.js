d261ca4f2dd56a320d0b63738b3dd409e2a70eb9
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-11-10 16:01:41 +0000

    wip

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBarTop from '../navbar.top';
 import Footer from '../footer';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import { ToastContainer, toast } from 'react-toastify';
 import './map.css';
 import './mapbox-v051.css';
 
@@ -13,7 +14,26 @@ class Map extends Component {
     this.baseState = this.state;
   }
 
+  login() {
+    this.props.auth.login();
+  }
+
+  onMapLoad = (map) => {    
+    // map.addControl(Navigation({
+    //   positionOptions: {
+    //     enableHighAccuracy: true
+    //   },
+    //   trackUserLocation: true
+    // }));
+  };
+
+  _onClickMap(map, evt) {
+    console.log(evt.lngLat);
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+
     const Map = ReactMapboxGl({
       accessToken: "pk.eyJ1IjoicmljY2FyZG9uZSIsImEiOiJqbXBIcDlFIn0.SuzRGlZwV_OJKyNH9DtJJg"
     });
@@ -22,15 +42,22 @@ class Map extends Component {
       <div>
         <NavBarTop auth={this.props.auth} {...this.props} />
         <div className="container">
-          <h1 className="page-title">Map</h1>
-          <div className="border-left">
-            <h3 className="page-subtitle">
+          <ToastContainer
+              hideProgressBar
+              newestOnTop
+          />
+          {
+            isAuthenticated() &&
+            <div>
+              <MapContainer>
               <Map
                   style="mapbox://styles/mapbox/streets-v9"
                   containerStyle={{
                   height: "100vh",
-                  width: "100vw"
+                  width: "100vw"                  
                 }}
+                  onStyleLoad={this.onMapLoad}
+                  onClick={this._onClickMap}
               >
                 <Layer
                     type="symbol"
@@ -40,8 +67,23 @@ class Map extends Component {
                   <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
                 </Layer>
               </Map>
-            </h3>
-          </div>
+              </MapContainer>
+              <br />
+            </div>
+          }
+          {
+            !isAuthenticated() &&
+            <h4>
+              You are not logged in! Please{' '}
+              <a
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.login.bind(this)}
+              >
+                Log In
+                </a>
+              {' '}to continue.
+              </h4>
+          }
         </div>
         <Footer />
       </div>
