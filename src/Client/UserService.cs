ef6e220e0a4a587c2bd65a1081ce3d9a7b782278
Riccardo Di Nuzzo <riccardo.dinuzzo@chubb.com>
2019-12-31 17:45:18 +0000

    implementing one page layout and grpc service (wip)

---
+++ src/Client/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatchMe.Web.Shared;
+
+namespace CatchMe.Web.Client
+{
+    public class UserService
+    {
+        private readonly List<Friend> _friends = new List<Friend>();
+        public IReadOnlyList<Friend> Friends => _friends;
+
+        public async Task InviteFriendAsync(string friendEmail, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
