e6e1147c05c54ad5fb29a331d816693aa016d938
Riccardo Di Nuzzo <riccardo.dinuzzo@chubb.com>
2020-01-18 17:09:19 +0000

    implemented youonmap view

--- src/Client/wwwroot/scripts/mapboxMng.js
+++ src/Client/wwwroot/scripts/mapboxMng.js
@@ -1,15 +1,36 @@
 ﻿window.mapBoxFunctions = {
     initMapBox: function () {
-        mapboxgl.accessToken = 'pk.eyJ1IjoicmljY2FyZG9uZSIsImEiOiJqbXBIcDlFIn0.SuzRGlZwV_OJKyNH9DtJJg';
-        window.myMap = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [34.047, 63.779],
-            zoom: 4.41
-        });
-        window.myMap.on('load', function () {
-            myMap.resize();
-        });
+        if ("geolocation" in navigator) {
+            mapboxgl.accessToken = 'pk.eyJ1IjoicmljY2FyZG9uZSIsImEiOiJqbXBIcDlFIn0.SuzRGlZwV_OJKyNH9DtJJg';
+            var defaultZoom = 14;
+            navigator.geolocation.getCurrentPosition(position => {
+                window.myMap = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: [position.coords.longitude, position.coords.latitude],
+                    zoom: defaultZoom
+                });
+                var geoLocate = new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+                    trackUserLocation: true
+                });
+                geoLocate.on('geolocate', function (e) {
+                    myMap.setZoom(defaultZoom);
+                });
+                window.myMap.addControl(geoLocate);
+                window.myMap.on('load',
+                    function () {
+                        myMap.resize();
+                        geoLocate.trigger();
+                        
+                    });
+            });
+        } else {
+            /* geolocation IS NOT available, handle it */
+            console.log("not available");
+        }
 
         return;
     }
