15ee722982a275862516c46bae0fd407ee5ed1ef
Riccardo Di Nuzzo <riccardo.dinuzzo@chubb.com>
2020-02-06 20:44:11 +0000

    implementing grpc service (wip)

--- src/Server/Controllers/UserController.cs
+++ src/Server/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazor.Auth0.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
---
+++ src/Server/Services/CurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using CatchMe.Web.Server.Protos;
+using Grpc.Core;
+
+namespace CatchMe.Web.Server.Services
+{
+    public class CurrentUserService : CurrentUser.CurrentUserBase
+    {
+        private readonly StartCurrentUserSessionReply _fakeResponse = new StartCurrentUserSessionReply();
+        public override Task<StartCurrentUserSessionReply> StartCurrentUserSession(StartCurrentUserSessionRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(_fakeResponse); // base.StartCurrentUserSession(request, context);
+        }
+    }
+}
